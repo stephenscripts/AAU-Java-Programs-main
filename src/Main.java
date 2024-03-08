9ed0a3c2e93de5535c657013b561dfa4c4e38d3f
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-03-08 09:34:04 +0100

    The new Class projects

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            double r, a;
+            System.out.println("Radius: ");
+            r = in.nextDouble();
+            final double pi = 22.0/7.0;
+            a = pi * r * r;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            System.out.println("The Area of the circle with radius " + r + ": " + a);
+
+
     }
 }
